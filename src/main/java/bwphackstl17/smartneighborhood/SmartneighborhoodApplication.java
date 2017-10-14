33393b96b82ca4276419e2829944f1bf65201b03
Sparrow <mel.e.grigg@gmail.com>
2017-10-14 08:26:52 -0500

    Initial commit

---
+++ src/main/java/bwphackstl17/smartneighborhood/SmartneighborhoodApplication.java
@@ -0,0 +1,12 @@
+package bwphackstl17.smartneighborhood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartneighborhoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartneighborhoodApplication.class, args);
+	}
+}
